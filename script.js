0c461d8d8ade7457e8f911b6a96b441ce7de95d0
KartiKBriteinno <153173760+KartiKBriteinno@users.noreply.github.com>
2024-05-11 16:47:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-//complete this code
-class Animal { constructor(species) {
+class Animal {
+    constructor(species) {
         this._species = species;
     }
 
@@ -9,15 +9,20 @@ class Animal { constructor(species) {
 
     makeSound() {
         console.log(`The ${this.species} makes a sound`);
-    }}
+    }
+}
 
-class Dog extends Animal {  purr() {
+class Cat extends Animal {
+    purr() {
         console.log("purr");
-    }}
+    }
+}
 
-class Cat extends Animal {bark() {
+class Dog extends Animal {
+    bark() {
         console.log("woof");
-    }}
+    }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
