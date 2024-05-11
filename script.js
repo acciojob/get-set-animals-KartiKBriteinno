2fb25d72f9603ed3edc9979f302a926b394d40bc
KartiKBriteinno <153173760+KartiKBriteinno@users.noreply.github.com>
2024-05-11 16:35:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-class Animal {
-    constructor(species) {
+//complete this code
+class Animal { constructor(species) {
         this._species = species;
     }
 
@@ -9,26 +9,17 @@ class Animal {
 
     makeSound() {
         console.log(`The ${this.species} makes a sound`);
-    }
-}
+    }}
 
-class Cat extends Animal {
-    purr() {
+class Dog extends Animal {  purr() {
         console.log("purr");
-    }
-}
+    }}
 
-class Dog extends Animal {
-    bark() {
+class Cat extends Animal {bark() {
         console.log("woof");
-    }
-}
+    }}
 
-// Example usage
-const myCat = new Cat("Siamese");
-myCat.makeSound(); // Output: The Siamese makes a sound
-myCat.purr(); // Output: purr
-
-const myDog = new Dog("Golden Retriever");
-myDog.makeSound(); // Output: The Golden Retriever makes a sound
-myDog.bark(); // Output: woof
+// Do not change the code below this line
+window.Animal = Animal;
+window.Dog = Dog;
+window.Cat = Cat;
